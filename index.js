8157be9870261d677a16f8f84385132451e7e16f
Max Mendes <maxmendes0509@gmail.com>
2021-05-17 11:02:20 -0300

    parse size fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = require('./src/routes')
 
+
 const app = express()
 
-app.use(express.json())
+app.use(express.json({limit: 100}))
 app.use(router)
-
 
 const port = process.env.PORT || '3030'
 
